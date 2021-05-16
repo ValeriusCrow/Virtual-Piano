175f903f9c3279fef7359b8b04abe4ea809795db
ValeriusCrow <77670739+ValeriusCrow@users.noreply.github.com>
2021-05-16 16:24:42 +0300

    Added black keys

--- script.js
+++ script.js
@@ -2,31 +2,39 @@ document.addEventListener("keydown", function (e) {
     if (e.code == "KeyA") {
         let audio = new Audio("sounds/A.mp3");
         audio.play();
-        console.log("The 'A' key is pressed.");
     } else if (e.code == "KeyS") {
         let audio = new Audio("sounds/S.mp3");
         audio.play();
-        console.log("The 'S' key is pressed.");
     } else if (e.code == "KeyD") {
         let audio = new Audio("sounds/D.mp3");
         audio.play();
-        console.log("The 'D' key is pressed.");
     } else if (e.code == "KeyF") {
         let audio = new Audio("sounds/F.mp3");
         audio.play();
-        console.log("The 'F' key is pressed.");
     } else if (e.code == "KeyG") {
         let audio = new Audio("sounds/G.mp3");
         audio.play();
-        console.log("The 'G' key is pressed.");
     } else if (e.code == "KeyH") {
         let audio = new Audio("sounds/H.mp3");
         audio.play();
-        console.log("The 'H' key is pressed.");
     } else if (e.code == "KeyJ") {
         let audio = new Audio("sounds/J.mp3");
         audio.play();
-        console.log("The 'J' key is pressed.");
+    } else if (e.code == "KeyW") {
+        let audio = new Audio("sounds/W.mp3");
+        audio.play();
+    } else if (e.code == "KeyE") {
+        let audio = new Audio("sounds/E.mp3");
+        audio.play();
+    } else if (e.code == "KeyT") {
+        let audio = new Audio("sounds/T.mp3");
+        audio.play();
+    } else if (e.code == "KeyY") {
+        let audio = new Audio("sounds/Y.mp3");
+        audio.play();
+    } else if (e.code == "KeyU") {
+        let audio = new Audio("sounds/U.mp3");
+        audio.play();
     } else {
         console.log("Wrong key!");
     }
